5cd5ad5de0473321e004c442bf5d16a96ab895ff
Jeremy Hough <jhough@leaffilter.com>
2017-01-11 16:39:31 -0500

    allow region to be used for hinting during parsing

--- src/PhoneNumber.php
+++ src/PhoneNumber.php
@@ -10,10 +10,16 @@ class PhoneNumber {
     self::$util = \libphonenumber\PhoneNumberUtil::getInstance();
     self::$initialized = true;
   }
-  public static function getFormattedNumber($input_string) {
+  public static function getFormattedNumber($input_string, $country = 'US') {
     self::initialize(); 
     try {
-      $number = self::$util->parse($input_string, "US");
+      /*
+       * Country is used internally during parsing to convert to E.164 format.
+       * It is used as a default region to fall back to when a number is formatted ambiguously.
+       * isValidNumber does not speak to whether a number is associated with the default region,
+       * but rather that the number is diallable
+       */
+      $number = self::$util->parse($input_string, $country);
       if ( self::$util->isValidNumber($number) ) {
         $formatted_number = preg_replace(self::$search_pattern, self::$replace_pattern, $number->getNationalNumber());
         if ( $formatted_number == $number->getNationalNumber() ) return $input_string;
